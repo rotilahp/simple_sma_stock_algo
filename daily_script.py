08c2a60810d9fa4c932167961f8e5d6cab40f2b7
Daniel <rotilahp@yahoo.com>
2020-05-13 17:20:48 -0700

    Added Inventory/Share count for stocks

--- daily_script.py
+++ daily_script.py
@@ -9,6 +9,12 @@ objectList=[]
 for stock in stocksList:
     objectList.append(myStock(stock))
 
+#set amount of stock to buy based on stock price
+def setInventory():
+    price = int(float(obj.currentPrice))
+    obj.inventory = int(30000/(len(stocksList))/price) 
+    print(obj.inventory)
+
 def writeToFile():
     with open('history.txt','a+',newline='\n') as file:
         content = '{},{},{},{}\n'.format(obj.ticker,obj.buyState,obj.currentDate,obj.currentPrice)
@@ -33,7 +39,8 @@ while flag:
         elif obj.buyState == True and obj.previousBuyState ==False:
             #this means we bought shares of this stock
             writeToFile()
+            setInventory()
             obj.previousBuyState = True
             obj.smaDayTradeSell() 
 
-        time.sleep(15)
+        time.sleep(15)        time.sleep(16)
