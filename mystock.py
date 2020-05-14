5f9445c7fb3f0468cc50cf96390480564c80a717
Daniel <rotilahp@yahoo.com>
2020-05-13 17:18:44 -0700

    Using Needle Algo and Inventory Support
    Switched algo to needle, which uses sma3, sma8, and sma21. Also added support for stock inventory to keep track of how many shares you have/want to buy.

--- mystock.py
+++ mystock.py
@@ -8,12 +8,12 @@ class myStock:
     outputSize = 10
     timeBtnCrosses=3
     timeAlertMax=3
-    smaTimePeriod=[3,21]
+    smaTimePeriod=[3,8,21]
     
     def __init__(self,ticker,stockInt='5min'):
         self._ticker=ticker
         self.stockInterval = stockInt
-        self.inventory=20
+        self._inventory=50
         self.technical='sma'
         self.mergeData={}
         self.smaList=[]
@@ -29,7 +29,7 @@ class myStock:
         url = "https://api.twelvedata.com/time_series?symbol={}&interval={}&format=JSON&apikey={}".format(self._ticker,self.stockInterval,twelveApi)
         response = requests.get(url)
         price = json.loads(response.text)
-        
+    
         for timePeriod in self.smaTimePeriod:
             url = 'https://api.twelvedata.com/{}?symbol={}&interval={}&time_period={}&format=JSON&apikey={}'.format(self.technical,self._ticker,self.stockInterval,timePeriod,twelveApi)
             response = requests.get(url)
@@ -54,30 +54,49 @@ class myStock:
             self.smaList.append(data)     
 
     def smaDayTradeBuy(self):
-        sma3 = self.smaList[0]
-        sma21 = self.smaList[1]
+        try:
+            sma3 = self.smaList[0]
+            sma8 = self.smaList[1]
+            sma21 = self.smaList[2]
 
-        df_sma3 = pd.DataFrame(sma3['values'])
-        df_sma3_price = df_sma3['sma']
+            df_sma3 = pd.DataFrame(sma3['values'])
+            df_sma3_price = df_sma3['sma']
 
-        df_sma21 = pd.DataFrame(sma21['values'])
-        df_sma21_price = df_sma21['sma']
+            df_sma8 = pd.DataFrame(sma8['values'])
+            df_sma8_price = df_sma8['sma']
 
-        if df_sma3_price[0] > df_sma21_price[0]:
-            self._buyState = True
-
+            df_sma21 = pd.DataFrame(sma21['values'])
+            df_sma21_price = df_sma21['sma']
+            print('if {} > {} and {} > {}'.format(df_sma3_price[0],df_sma21_price[0],df_sma3_price[0],df_sma8_price[0]))
+            if df_sma3_price[0] > df_sma21_price[0] and df_sma3_price[0] > df_sma8_price[0]:
+                self._buyState = True
+        except:
+            print('missing a list')
+            pass
+        print(self._buyState)
     def smaDayTradeSell(self):
-        sma3 = self.smaList[0]
-        sma21 = self.smaList[1]
+        try:
+            sma3 = self.smaList[0]
+            sma8 = self.smaList[1]
+            sma21 = self.smaList[2]
+ 
+            df_sma3 = pd.DataFrame(sma3['values'])
+            df_sma3_price = df_sma3['sma']
 
-        df_sma3 = pd.DataFrame(sma3['values'])
-        df_sma3_price = df_sma3['sma']
+            df_sma8 = pd.DataFrame(sma8['values'])
+            df_sma8_price = df_sma8['sma']
 
-        df_sma21 = pd.DataFrame(sma21['values'])
-        df_sma21_price = df_sma21['sma']
+            df_sma21 = pd.DataFrame(sma21['values'])
+            df_sma21_price = df_sma21['sma']
 
-        if df_sma3_price[0] < df_sma21_price[0]:
-            self._buyState = False
+            print('if {} < {} or {} < {}'.format(df_sma3_price[0],df_sma21_price[0],df_sma3_price[0],df_sma8_price[0]))
+            if df_sma3_price[0] < df_sma21_price[0] or df_sma3_price[0] < df_sma8_price[0]:
+                self._buyState = False
+        except:
+            print('missing an sma list')
+            pass
+
+        
 
     @property
     def buyState(self):
@@ -102,6 +121,17 @@ class myStock:
             print('need a bool fool')
 
     @property
+    def inventory(self):
+        return self._inventory
+
+    @inventory.setter
+    def inventory(self,value):
+        if type(value) == int:
+            self._inventory=value
+        else:
+            print('need an int fool')
+
+    @property
     def currentDate(self):
         _date = pd.DataFrame(self.mergeData['values'])['datetime'][0]
         self._currentDate = _date
@@ -116,6 +146,3 @@ class myStock:
     @property
     def ticker(self):
         return self._ticker
-        
-
-
